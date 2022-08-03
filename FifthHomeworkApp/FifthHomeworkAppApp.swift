fca3728a01e0747b41f9f003e25dbb6ee4b5ab4a
anton <otlichnonick@yandex.ru>
2022-08-03 08:34:42 +0300

    Initial Commit

---
+++ FifthHomeworkApp/FifthHomeworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifthHomeworkAppApp.swift
+//  FifthHomeworkApp
+//
+//  Created by Anton Agafonov on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FifthHomeworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
