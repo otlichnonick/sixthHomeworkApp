b0a660d47e39fdfca76b9176ac8edfe5bc819199
anton <otlichnonick@yandex.ru>
2022-08-05 07:18:20 +0300

    Починил отображение суффиксов и поиск по ним

--- FifthHomeworkApp/ViewModel.swift
+++ FifthHomeworkApp/ViewModel.swift
@@ -12,30 +12,41 @@ class ViewModel: ObservableObject {
     @Published var text: String = "Сегодня солнце зашло за тучи, сегодня волны бьют так больно, я видел, как умирает надежда Ямайки - моя душа плачет! Какая боль, какая боль! Аргентина-Ямайка: 5-0"
     @Published private var sortIsAscending: Bool = true
     @Published var searchText: String = ""
-    @Published var sortedWholeListSuffix: [(String, Int)] = .init()
-    @Published var sortedTopTenSuffixList: [(String, Int)] = .init()
-
+    @Published var searchSuffix: String = ""
+    @Published var wholeListSuffix: [(String, Int)] = .init()
+    @Published var topTenSuffixList: [(String, Int)] = .init()
     private var bag: Set<AnyCancellable> = .init()
     
-    private var wholeListSuffix: [(String, Int)] {
-        calculateSuffixesNumber()
-            .sorted { sortIsAscending ? $0.0 < $1.0 : $0.0 > $1.0 }
+    var sortedWholeListSuffix: [(String, Int)] {
+        searchSuffix.isEmpty ? wholeListSuffix : wholeListSuffix.filter({ $0.0.contains(searchSuffix.lowercased()) })
     }
     
-    private var topTenSuffixList: [(String, Int)] {
-        calculateSuffixesNumber()
-            .sorted(by: { $0.1 > $1.1 })
-            .prefix(10)
-            .sorted { $0.1 == $1.1 ? $0.0 < $1.0 : false }
+    var sortedTopTenSuffixList: [(String, Int)] {
+        searchSuffix.isEmpty ? topTenSuffixList : topTenSuffixList.filter({ $0.0.contains(searchSuffix.lowercased()) })
     }
     
-    init() {
+    init() { bindings() }
+    
+    private func bindings() {
+        $text
+            .sink { [weak self] newText in
+                guard let strongSelf = self else { return }
+                
+                strongSelf.wholeListSuffix = strongSelf.calculateSuffixesNumber(from: newText)
+                    .sorted { strongSelf.sortIsAscending ? $0.0 < $1.0 : $0.0 > $1.0 }
+                
+                strongSelf.topTenSuffixList = strongSelf.calculateSuffixesNumber(from: newText)
+                    .sorted(by: { $0.1 > $1.1 })
+                    .prefix(10)
+                    .sorted { $0.1 == $1.1 ? $0.0 < $1.0 : false }
+            }
+            .store(in: &bag)
+        
         $searchText
             .debounce(for: .milliseconds(500), scheduler: DispatchQueue.main)
             .sink(receiveValue: { [weak self] value in
                 guard let strongSelf = self else { return }
-                strongSelf.sortedWholeListSuffix = value.isEmpty ? strongSelf.wholeListSuffix : strongSelf.wholeListSuffix.filter({ $0.0.contains(value.lowercased()) })
-                strongSelf.sortedTopTenSuffixList = value.isEmpty ? strongSelf.topTenSuffixList : strongSelf.topTenSuffixList.filter({ $0.0.contains(value.lowercased()) })
+                strongSelf.searchSuffix = value
             })
             .store(in: &bag)
     }
@@ -48,9 +59,9 @@ class ViewModel: ObservableObject {
         sortIsAscending = false
     }
     
-    private func calculateSuffixesNumber() -> [(String, Int)] {
+    private func calculateSuffixesNumber(from text: String) -> [(String, Int)] {
         var store: [(string: String, int: Int)] = .init()
-        for string in getSuffixes() {
+        for string in getSuffixes(from: text) {
             if store.contains(where: { $0.string == string }) {
                 let index = store.firstIndex(where: { $0.string == string })!
                 store[index].int += 1
@@ -62,7 +73,7 @@ class ViewModel: ObservableObject {
         return store
     }
     
-    private func getSuffixes() -> [String] {
+    private func getSuffixes(from text: String) -> [String] {
         text
             .components(separatedBy: .init(charactersIn: ",-!?;: "))
             .filter({ $0.count > 2 })
